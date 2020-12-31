ab1d30e012c7791efba69ce985ef3968a9d06fb9
Rishab Singh <main.rishab@gmail.com>
2020-12-31 20:37:53 +0530

    Website Uploaded

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The nth term of an A.P. is given by an = 3 + 4n. The common difference is",
+    answer: "4", // Answer and The Option with the correct answer should be pixel perfect
+    options: [
+      "7",
+      "3", // Answer and the correct option
+      "4",
+      "1"
+    ]
+  },
+    {
+    numb: 2,
+    question: "If p, q, r and s are in A.P. then r – q is",
+    answer: "s - r",
+    options: [
+      "s - p",
+      "s - q",
+      "s - r",
+      "None of these"
+    ]
+  },
+    {
+    numb: 3,
+    question: "If the sum of three numbers in an A.P. is 9 and their product is 24, then numbers are",
+    answer: "2, 3, 4",
+    options: [
+      "2, 4, 6",
+      "1, 5, 3",
+      "2, 8, 4",
+      "2, 3, 4"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The (n – 1)th term of an A.P. is given by 7,12,17, 22,… is",
+    answer: "5n – 3",
+    options: [
+      "5n + 2",
+      "5n + 3",
+      "5n – 5",
+      "5n – 3"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The nth term of an A.P. 5, 2, -1, -4, -7 … is",
+    answer: "8 – 3n",
+    options: [
+      "2n + 5",
+      "2n - 5",
+      "8 – 3n",
+      "3n – 8"
+    ]
+  },
+    {
+    numb: 6,
+    question: "The 10th term from the end of the A.P. -5, -10, -15,…, -1000 is",
+    answer: "-955",
+    options: [
+      "-955",
+      "-945",
+      "-950",
+      "-965"
+    ]
+  },
+    {
+  numb: 7,
+  question: " Find the sum of 12 terms of an A.P. whose nth term is given by an = 3n + 4",
+  answer: "262",
+  options: [
+    "262",
+    "272",
+    "282",
+    "292"
+    ]
+  },
+    {
+  numb: 8,
+  question: "The sum of all two digit odd numbers is",
+  answer: "2475",
+  options: [
+    "2575",
+    "2475",
+    "2524",
+    "2425"
+    ]
+  },
+    {
+  numb: 9,
+  question: "The sum of first n odd natural numbers is",
+  answer: "n²",
+  options: [
+    "2n²",
+    "2n + 1",
+    "2n – 1",
+    "n²"
+    ]
+  },
+    {
+  numb: 10,
+  question: "The number of multiples lie between n and n² which are divisible by n is",
+  answer: "n - 2",
+  options: [
+    "n + 1",
+    "n",
+    "n - 1",
+    "n - 2"
+    ]
+  }
+];
